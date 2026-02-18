3899f897b0967ee6538ed76ddd240fcbe4b177dd
Josiah Ricketts <jrickett2@lsoc.org>
2026-02-18 09:24:32 -0600

    hello texture

--- src/helloTexture.cpp
+++ src/helloTexture.cpp
@@ -1,139 +1,133 @@
 #include "shader.h"
-#include <cstddef>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <stdio.h>
+#include <cstddef>
 #include <iostream>
 #include <stb_image.h>
 
+const int windowWidth = 800;
+const int windowHeight = 600;
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow* window);
 
-float vertices[] = {
-	-0.5f, -0.5f, 0.0f,	//bottom left
-	0.5f, -0.5f, 0.0f, //bottom right
-	0.0f,  0.5f, 0.0f //top
+const float vertices[] = {
+	//vertex positions		//tex coords
+    -0.5f, -0.5f, 0.0f,		0.0f, 0.0f, //bottom left
+     0.5f, -0.5f, 0.0f,		1.0f, 0.0f, //bottom right
+     0.5f,  0.5f, 0.0f,		1.0f, 1.0f, //top right
+     -0.5f,  0.5f, 0.0f,	0.0f, 1.0f, //top left
 };
 
-float texCoords[] = {
-	0.0f, 0.0f,
-	1.0f, 0.0f, 
-	0.5f, 1.0f
+
+
+
+const int indices[] = {
+	0, 1, 2,
+	0, 3, 2,
 };
 
-unsigned int indices[] = {
-	0, 1, 2
-};
+ void framebuffer_size_callback(GLFWwindow* winodw, int width, int height);
 
-int main() {
+int main(){
 
-	// Initialize and create window
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGl", NULL, NULL);
-	if (window == NULL){
-		std::cout << "Failed to create Glfw window" << std::endl;
-		glfwTerminate();
+
+	GLFWwindow* window;
+
+	window = glfwCreateWindow(windowWidth, windowHeight, "MyWindow", NULL, NULL);
+	if(window == NULL){
+		std::cout << "Failed to create Window" << std::endl;
 		return -1;
 	}
 
-
-	//Grab context and viewport for drawing
 	glfwMakeContextCurrent(window);
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}    
 
-	glViewport(0, 0, 800, 600);
+	glViewport(0, 0, windowWidth, windowHeight);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	//******************************Initialization******************************
+	//################################################################################
 
 	unsigned int VAO;
-
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
 
-	unsigned int VBO, TBO;
-
+	unsigned int VBO;
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) 0);
-	glEnableVertexAttribArray(0);
-
-	glGenBuffers(1, &TBO);
-	glBindBuffer(GL_ARRAY_BUFFER, TBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(texCoords), texCoords, GL_STATIC_DRAW);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*) 0);
-	glEnableVertexAttribArray(1);
 
 	unsigned int EBO;
 	glGenBuffers(1, &EBO);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*) 0); //vertex positions
+	glEnableVertexAttribArray(0);
 
-	//******************************load image******************************
-	unsigned int texture;
-	glGenTextures(1, &texture);
-	glBindTexture(GL_TEXTURE_2D, texture);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float))); //texture coordinates
+	glEnableVertexAttribArray(1);
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	//################################################################################
 
-	int width, height, nrChannels;
-	unsigned char *data = stbi_load("assets/container.jpg", &width, &height, &nrChannels, 0); 
+	unsigned char* data;
+	int width, height, channels;
+	data = stbi_load("assets/container.jpg", &width, &height, &channels, 0);
+
 	if(!data){
-		std::cout << "Failed to load texture" << std::endl;
+		std::cout << "Image failed to load" << std::endl;
 		return -1;
 	}
 
+
+
+	unsigned int texture;
+	glGenTextures(1, &texture);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	glBindTexture(GL_TEXTURE_2D, texture);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+	glGenerateMipmap(GL_TEXTURE_2D);
 
 	stbi_image_free(data);
 
 
 
-	Shader shader = Shader("shader/vertex/texture.vert", "shaders/fragment/texture.frag");
+	
+	
+	Shader shader("shaders/vertex/texture.vert", "shaders/fragment/texture.frag");
 
-
-	//main render loop	
-	while(!glfwWindowShouldClose(window))
-	{
-		processInput(window);
-
+	while(!glfwWindowShouldClose(window)){
 		glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		shader.use();
-
 		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+		shader.use();
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
-		glfwPollEvents();    
+		glfwPollEvents();
+
 	}
 
 	glfwTerminate();
 	return 0;
+
+
+
+
 }
 
-
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height){
-	glViewport(0, 0, width, height);
-}
-
-void processInput(GLFWwindow* window){
-	if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
-		glfwSetWindowShouldClose(window, true);
+ void framebuffer_size_callback(GLFWwindow* winodw, int width, int height){
+		glViewport(-1, 0, width, height);
 	}
-}
--- src/shaders/vertex/texture.vert
+++ src/shaders/vertex/texture.vert
@@ -1,14 +1,12 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
-layout (location = 2) in vec2 aTexPos;
+layout (location = 1) in vec2 aTexPos;
 
-out vec3 outColor;
 out vec2 texCoord;
 
 void main()
 {
 	gl_Position = vec4(aPos, 1.0);
-	outColor = aColor;
 	texCoord = aTexPos;
 }
 
